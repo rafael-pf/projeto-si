6bff2041be994ed01fc4969f292cea9083367348
rafael-pf <rpf@cin.ufpe.br>
2026-04-27 22:10:50 -0300

    bfs

--- sketch.js
+++ sketch.js
@@ -20,19 +20,6 @@ let food;
 let mudField = {};
 let waterField = {};
 
-
-function checkColision() {
-    distance = Math.sqrt((posX - vehicle.pos.x) ** 2 + (posY - vehicle.pos.y) ** 2);
-
-
-    if (distance < 2) {
-        counter++;
-        posX = Math.floor(Math.random() * 501);
-        posY = Math.floor(Math.random() * 501);
-        target = createVector(posX, posY);
-    }
-}
-
 function drawGrid() {
     for (let x = 0; x < width; x += gridSize) {
         for (let y = 0; y < height; y += gridSize) {
@@ -59,10 +46,10 @@ function generateGrid() {
 // function to generate a obstacle
 function generateObstacle() {
     let obstacle = {
-        x: Math.floor(Math.random() * width / 50) * 50,
-        y: Math.floor(Math.random() * height / 50) * 50,
-        width: 50 * Math.floor(Math.random() * 2 + 2),
-        height: 50 * Math.floor(Math.random() * 2 + 2)
+        x: Math.floor(Math.random() * width / gridSize) * gridSize,
+        y: Math.floor(Math.random() * height / gridSize) * gridSize,
+        width: gridSize * Math.floor(Math.random() * 2 + 2),
+        height: gridSize * Math.floor(Math.random() * 2 + 2)
     }
 
     // mark the grid points that are inside the obstacle
@@ -79,10 +66,10 @@ function generateObstacle() {
 //function to generate terrain
 function generateTerrain(tag) {
     let terrain = {
-        x: Math.floor(Math.random() * width / 50) * 50,
-        y: Math.floor(Math.random() * height / 50) * 50,
-        width: 50 * Math.floor(Math.random() * 4 + 3),
-        height: 50 * Math.floor(Math.random() * 4 + 3)
+        x: Math.floor(Math.random() * width / gridSize) * gridSize,
+        y: Math.floor(Math.random() * height / gridSize) * gridSize,
+        width: gridSize * Math.floor(Math.random() * 4 + 3),
+        height: gridSize * Math.floor(Math.random() * 4 + 3)
     }
 
     // mark the grid points that are inside the terrain
@@ -99,10 +86,10 @@ function generateTerrain(tag) {
 // function to generate a object
 function generateObject() {
     let object = {
-        x: Math.floor(Math.random() * width / 50) * 50,
-        y: Math.floor(Math.random() * height / 50) * 50,
-        width: 50,
-        height: 50
+        x: Math.floor(Math.random() * width / gridSize) * gridSize,
+        y: Math.floor(Math.random() * height / gridSize) * gridSize,
+        width: gridSize,
+        height: gridSize
     }
 
     return object;
@@ -142,23 +129,23 @@ function setup() {
     food = generateFood();
     mudField = generateTerrain(2);
     waterField = generateTerrain(3);
-    vehicle = new Vehicle(obstacles);
+    vehicle = new Vehicle(obstacles, grid, gridSize);
+    vehicle.seek(food);
     counter = 0;
 }
 
 function draw() {
-    background(0);
+    background('#F2D16B');
 
     for (let i = 0; i < obstacles.length; i++) {
-        drawRect(obstacles[i], '#808080');
+        drawRect(obstacles[i], '#333333');
     }
-    drawRect(food, '#FFD700');
     drawRect(mudField, '#765432');
     drawRect(waterField, '#4A90E2');
+    drawRect(food, '#FF8C00');
     drawGrid();
 
     // vehicle.seek(target);
     vehicle.update();
     vehicle.show();
-
 }
--- vehicle.js
+++ vehicle.js
@@ -10,50 +10,114 @@
 // Pursue: https://editor.p5js.org/codingtrain/sketches/XbsgoU_of
 
 class Vehicle {
-    constructor(obstacles) {
+    constructor(obstacles, grid, gridSize) {
         // generate random position
 
-        let x = Math.floor(Math.random() * width / 50) * 50;
-        let y = Math.floor(Math.random() * height / 50) * 50;
+        let x = Math.floor(Math.random() * width / gridSize) * gridSize;
+        let y = Math.floor(Math.random() * height / gridSize) * gridSize;
 
         for (let i = 0; i < obstacles.length; i++) {
             if (x >= obstacles[i].x && x < obstacles[i].x + obstacles[i].width &&
                 y >= obstacles[i].y && y < obstacles[i].y + obstacles[i].height) {
-                x = Math.floor(Math.random() * width / 50) * 50;
-                y = Math.floor(Math.random() * height / 50) * 50;
+                x = Math.floor(Math.random() * width / gridSize) * gridSize;
+                y = Math.floor(Math.random() * height / gridSize) * gridSize;
                 i = -1;
             }
         }
 
-
+        this.grid = grid;
+        this.obstacles = obstacles;
+        this.path = null;
+        this.visited = [];
+        this.frontier = [];
         this.pos = createVector(x, y);
         this.vel = createVector(0, 0);
         this.acc = createVector(0, 0);
         this.pointer = createVector(x + 50, y + 20)
         this.maxSpeed = 4;
         this.maxForce = 0.25;
-        this.size = 50;
+        this.size = gridSize;
         this.r = 16;
         this.searchRadius = 150;
     }
 
-    seek(target) {
-        let force = p5.Vector.sub(target, this.pos);
-        // outside search radius
-        if (force.mag() > this.searchRadius) {
-            // explore
-            force = p5.Vector.sub(this.pointer, this.pos)
-            force.setMag(this.maxSpeed);
-            force.sub(this.vel);
-            force.limit(this.maxForce);
-            this.applyForce(force);
-        } else {
-            // inside search radius
-            force.setMag(this.maxSpeed);
-            force.sub(this.vel);
-            force.limit(this.maxForce);
-            this.applyForce(force);
+    getNeighbors(current) {
+        let neighbors = [];
+        let directions = [
+            createVector(0, -gridSize),
+            createVector(gridSize, 0),
+            createVector(0, gridSize),
+            createVector(-gridSize, 0)
+        ];
+
+        for (let i = 0; i < directions.length; i++) {
+            let neighbor = p5.Vector.add(current, directions[i]);
+            if (neighbor.x >= 0 && neighbor.x < width && neighbor.y >= 0 && neighbor.y < height) {
+                // check if the neighbor is not an obstacle
+                let isObstacle = false;
+                for (let j = 0; j < this.obstacles.length; j++) {
+                    if (neighbor.x >= this.obstacles[j].x && neighbor.x < this.obstacles[j].x + this.obstacles[j].width &&
+                        neighbor.y >= this.obstacles[j].y && neighbor.y < this.obstacles[j].y + this.obstacles[j].height) {
+                        isObstacle = true;
+                        break;
+                    }
+                }
+                if (!isObstacle) {
+                    neighbors.push(neighbor);
+                }
+            }
         }
+
+        return neighbors;
+    }
+
+    seek(food) {
+        // BFS for food search
+        let start = this.pos;
+        let frontier = [];
+        frontier.push(start);
+        let came_from = {};
+        came_from[`${start.x},${start.y}`] = null;
+
+        while (frontier.length > 0) {
+            setTimeout(() => { }, 500);
+            let current = frontier.shift();
+            this.visited.push(current);
+
+            if (current.x === food.x && current.y === food.y) {
+                break;
+            }
+
+            let neighbors = this.getNeighbors(current);
+            this.frontier = neighbors;
+            for (let i = 0; i < neighbors.length; i++) {
+                let next = neighbors[i];
+                if (!came_from.hasOwnProperty(`${next.x},${next.y}`)) {
+                    frontier.push(next);
+                    came_from[`${next.x},${next.y}`] = current;
+                }
+            }
+        }
+
+        // reconstruct path
+        let path = [];
+        let current = food;
+        while (current != null) {
+            path.push(current);
+            current = came_from[`${current.x},${current.y}`];
+        }
+        this.path = path.reverse();
+
+        // // follow the path
+        // if (path.length > 1) {
+        //     let target = path[1];
+        //     let desired = p5.Vector.sub(target, this.pos);
+        //     desired.setMag(this.maxSpeed);
+        //     let steer = p5.Vector.sub(desired, this.vel);
+        //     steer.limit(this.maxForce);
+        //     this.applyForce(steer);
+        // }
+
     }
 
     applyForce(force) {
@@ -66,28 +130,48 @@ class Vehicle {
         this.vel.limit(this.maxSpeed);
         this.pos.add(this.vel);
         this.acc.set(0, 0);
-        this.edges();
+        // this.edges();
     }
 
     show() {
+        // print path
+        // fill(255);
+        // if (this.path != null) {
+        //     for (let i = 1; i < this.path.length - 1; i++) {
+        //         square(this.path[i].x, this.path[i].y, this.size);
+        //     }
+        // }
+
+        // print visited
+        fill(255, 0, 0, 100);
+        for (let i = 0; i < this.visited.length; i++) {
+            square(this.visited[i].x, this.visited[i].y, this.size);
+        }
+
+        // print frontier
+        fill(0, 255, 0, 100);
+        for (let i = 0; i < this.frontier.length; i++) {
+            square(this.frontier[i].x, this.frontier[i].y, this.size);
+        }
+
         stroke(255);
         strokeWeight(2);
         fill(0)
         square(this.pos.x, this.pos.y, this.size);
-        fill(255);
+
         pop();
     }
 
-    edges() {
-        if (this.pos.x > width + this.r) {
-            this.pos.x = -this.r;
-        } else if (this.pos.x < -this.r) {
-            this.pos.x = width + this.r;
-        }
-        if (this.pos.y > height + this.r) {
-            this.pos.y = -this.r;
-        } else if (this.pos.y < -this.r) {
-            this.pos.y = height + this.r;
-        }
-    }
+    // edges() {
+    //     if (this.pos.x > width + this.r) {
+    //         this.pos.x = -this.r;
+    //     } else if (this.pos.x < -this.r) {
+    //         this.pos.x = width + this.r;
+    //     }
+    //     if (this.pos.y > height + this.r) {
+    //         this.pos.y = -this.r;
+    //     } else if (this.pos.y < -this.r) {
+    //         this.pos.y = height + this.r;
+    //     }
+    // }
 }
